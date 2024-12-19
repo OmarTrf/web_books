742cfae0a9291349c989192c708b4bea7a3ad53f
OmarTrf <omartoraif803@gmail.com>
2024-12-19 05:15:00 +0100

    Improving The Nav And Header Design And Add The Search Bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// ----------------- NAV MENU ---------------------
+let nav_menu = document.querySelector('nav');
+
+// 1 - dynamically add and remove the active class based on the user's scroll position.
+document.addEventListener('scroll',function(e) {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > 220){
+        nav_menu.classList.add('active');
+    }else{
+        nav_menu.classList.remove('active');
+    }
+});
+
 // Show / Hide The Menu List Of Cart
 let cart_icon = document.querySelector('nav .icons .cart_book');
 let wish_icon = document.querySelector('nav .icons .cart_wishlist');
@@ -32,3 +45,25 @@ if(cart_icon && wish_icon && list_cart){
 
     });
 }
+
+
+
+
+
+
+
+let search_boxes = document.querySelectorAll('.search_box');
+
+// SEARCHE BOXE 
+// 1 - Button Select Category
+search_boxes.forEach(search_box=>{
+    if(search_box){
+        let button_main_category = search_box.querySelector('.select_box .main_item');
+        let menu = search_box.querySelector('.select_box .menu');
+    
+        button_main_category.addEventListener('click', function(e){
+            e.preventDefault();
+            menu.classList.toggle('active');
+        });
+    }
+})
